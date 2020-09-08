d1b8690da5125b2821777a12f35fc0943c0ba875
George Steel <george@net-glue.co.uk>
2020-09-08 11:45:06 +0100

    Change configuration so that the console commands are keyed with 'laminas-cli', thereby integrating with the laminas cli package and create an integration test to make sure that all the expected commands are available in the cli application that would be returned by the application factory shipped with laminas-cli

---
+++ tests/LaminasCliIntegrationTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netglue\PsrContainer\MessengerTest;
+
+use Laminas\Cli\ApplicationFactory;
+use Laminas\ConfigAggregator\ArrayProvider;
+use Laminas\ConfigAggregator\ConfigAggregator;
+use Laminas\ServiceManager\ServiceManager;
+use Netglue\PsrContainer\Messenger\ConfigProvider;
+use Netglue\PsrContainer\Messenger\FailureCommandsConfigProvider;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Messenger\MessageBus;
+use Symfony\Component\Messenger\Transport\Sync\SyncTransport;
+
+final class LaminasCliIntegrationTest extends TestCase
+{
+    /** @var Application */
+    private $cliApplication;
+    /** @var ServiceManager */
+    private $container;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+
+        $this->cliApplication = (new ApplicationFactory())($this->getContainer());
+    }
+
+    private function getContainer() : ContainerInterface
+    {
+        if ($this->container) {
+            return $this->container;
+        }
+
+        $aggregator = new ConfigAggregator([
+            ConfigProvider::class,
+            FailureCommandsConfigProvider::class,
+            new ArrayProvider([
+                'symfony' => [
+                    'messenger' => ['failure_transport' => 'failure'],
+                ],
+                'dependencies' => [
+                    'services' => [
+                        'failure' => new SyncTransport(new MessageBus()),
+                    ],
+                ],
+            ]),
+        ]);
+
+        $config = $aggregator->getMergedConfig();
+        $dependencies = $config['dependencies'];
+        $dependencies['services']['config'] = $config;
+        $this->container = new ServiceManager($dependencies);
+
+        return $this->container;
+    }
+
+    /** @return iterable<string, string[]> */
+    public function expectedCommandNameDataProvider(): iterable
+    {
+        $config = $this->getContainer()->get('config')['laminas-cli']['commands'];
+        foreach ($config as $commandName => $identifier) {
+            yield $commandName => [$commandName];
+        }
+    }
+
+    /** @dataProvider expectedCommandNameDataProvider */
+    public function testCommandsAreAvailableToTheCliApplication(string $commandName) : void
+    {
+        self::assertTrue($this->cliApplication->has($commandName));
+    }
+}
